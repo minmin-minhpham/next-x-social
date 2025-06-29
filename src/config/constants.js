7519157ca7da0c6900648bd57a284e6eaae5429e
min min <minh26102111@gmail.com>
2025-06-29 09:45:38 +0700

    feat(config): Media

---
+++ src/config/constants.js
@@ -0,0 +1,10 @@
+import { FlagIcon } from "@/components/icons";
+
+// MEDIA_SETTING
+export const MEDIA_SETTINGS_TABS = [
+  { id: 0, title: "Crop Image", name: "Crop" },
+  { id: 1, title: "Description Image", name: "Alt" },
+  { id: 2, title: "Warning", icon: FlagIcon },
+];
+
+export const WARNING_OPTIONS = ["nudity", "violence", "sensitive"];
