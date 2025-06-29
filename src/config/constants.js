963467f67b79940cf2dd8fdaebc381ba1bebc937
min min <minh26102111@gmail.com>
2025-06-29 09:47:10 +0700

    feat(constants): Auth

--- src/config/constants.js
+++ src/config/constants.js
@@ -8,3 +8,26 @@ export const MEDIA_SETTINGS_TABS = [
 ];
 
 export const WARNING_OPTIONS = ["nudity", "violence", "sensitive"];
+
+// AUTH
+export const SIGN_UP_NAV_ITEMS = [
+  { label: "About", url: "#" },
+  { label: "Download the X app", url: "#" },
+  { label: "Grok", url: "#" },
+  { label: "Help Center", url: "#" },
+  { label: "Terms of Service", url: "#" },
+  { label: "Privacy Policy", url: "#" },
+  { label: "Cookie Policy", url: "#" },
+  { label: "Accessibility", url: "#" },
+  { label: "Ads info", url: "#" },
+  { label: "Blog", url: "#" },
+  { label: "Careers", url: "#" },
+  { label: "Brand Resources", url: "#" },
+  { label: "Advertising", url: "#" },
+  { label: "Marketing", url: "#" },
+  { label: "X for Business", url: "#" },
+  { label: "Developers", url: "#" },
+  { label: "Directory", url: "#" },  
+  { label: "Settings", url: "#" },
+  { label: "©2025 M&M", url: "#" },
+];
